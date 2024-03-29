5cc617109b44e6f70b448fad705ccaf57b927c76
pro3068 <prajyotnaik1111@gmail.com>
2024-03-29 20:55:06 +0530

    first commit

---
+++ star.py
@@ -0,0 +1,22 @@
+"""for i in range(10):
+    for j in range(10):
+        print("*" ,end=" ")
+    print()
+    
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(f"{j}*{i}={i*j}", end="\t")
+    print()"""
+    
+
+"""for i in range(10):
+    for j in range(65, 65+i):
+        print(chr(j), end="")
+    print()"""
+    
+"""for i in range(5):
+    for j in range(2*i+1):
+        print(chr(65+j),end="")
+    print()"""
+    
+    
